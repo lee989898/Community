facac45b812dd1deb9fe85c7fa9dc43a66366aff
lee989898 <wnsrud1352@gmail.com>
2021-09-09 16:44:59 +0900

    주사위 이미지 추가

--- DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,22 +15,20 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
 
-        rollButton.setOnClickListener{
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
 
     }
 
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+       val diceImage: ImageView = findViewById(R.id.imageView)
+        diceImage.setImageResource(R.drawable.dice_2)
     }
 
-    class Dice(private val numSides: Int){
+    class Dice(private val numSides: Int) {
 
-        fun roll(): Int{
+        fun roll(): Int {
             return (1..numSides).random()
         }
 
