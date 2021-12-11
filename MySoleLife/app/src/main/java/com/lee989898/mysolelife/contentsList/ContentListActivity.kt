8a70995c023b2afe8a5fc0ec32aa0a8b60adcef9
lee989898 <wnsrud1352@gmail.com>
2021-12-11 22:30:17 +0900

    commit

---
+++ MySoleLife/app/src/main/java/com/lee989898/mysolelife/contentsList/ContentListActivity.kt
@@ -0,0 +1,14 @@
+package com.lee989898.mysolelife.contentsList
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lee989898.mysolelife.R
+
+class ContentListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_list)
+    }
+}
