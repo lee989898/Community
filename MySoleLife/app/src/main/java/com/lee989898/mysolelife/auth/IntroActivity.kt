3b63b42be3d895cab1f88f536eb8866a16d4f62a
lee989898 <wnsrud1352@gmail.com>
2021-12-11 19:23:45 +0900

    commit

--- MySoleLife/app/src/main/java/com/lee989898/mysolelife/auth/IntroActivity.kt
+++ MySoleLife/app/src/main/java/com/lee989898/mysolelife/auth/IntroActivity.kt
@@ -3,15 +3,24 @@ package com.lee989898.mysolelife.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lee989898.mysolelife.MainActivity
 import com.lee989898.mysolelife.R
 import com.lee989898.mysolelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
+
     lateinit var binding : ActivityIntroBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        auth = Firebase.auth
 
         binding = ActivityIntroBinding.inflate(layoutInflater)
 
@@ -29,6 +38,24 @@ class IntroActivity : AppCompatActivity() {
         }
 
         binding.noAccountBtn.setOnClickListener {
+
+            auth.signInAnonymously()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+
+
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        startActivity(intent)
+
+
+                    } else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+
+                    }
+                }
 
         }
 
