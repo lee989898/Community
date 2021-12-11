6d84753a7abf37fa9fb7e51dc5d35786d833cab9
lee989898 <wnsrud1352@gmail.com>
2021-12-11 22:40:43 +0900

    commit

---
+++ MySoleLife/app/src/main/java/com/lee989898/mysolelife/contentsList/ContentRVAdapter.kt
@@ -0,0 +1,31 @@
+package com.lee989898.mysolelife.contentsList
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.lee989898.mysolelife.R
+
+class ContentRVAdapter(val items: ArrayList<String>): RecyclerView.Adapter<ContentRVAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentRVAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ContentRVAdapter.ViewHolder, position: Int) {
+        holder.bindItems(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+        fun bindItems(item: String){
+
+        }
+
+    }
+
+}
