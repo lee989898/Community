0c2a1d54e31b7f030f8cc5f602e65ad308e5f481
lee989898 <wnsrud1352@gmail.com>
2021-12-12 20:32:20 +0900

    commit

--- MySoleLife/app/src/main/java/com/lee989898/mysolelife/auth/IntroActivity.kt
+++ MySoleLife/app/src/main/java/com/lee989898/mysolelife/auth/IntroActivity.kt
@@ -1,12 +1,30 @@
 package com.lee989898.mysolelife.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lee989898.mysolelife.R
+import com.lee989898.mysolelife.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        binding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.joinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
