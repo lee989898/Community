f51393b7d417e3c31f238136932a0133792905f4
lee989898 <wnsrud1352@gmail.com>
2021-12-12 22:58:46 +0900

    commit

--- MySoleLife/app/src/main/java/com/lee989898/mysolelife/fragments/HomeFragment.kt
+++ MySoleLife/app/src/main/java/com/lee989898/mysolelife/fragments/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.findNavController
 import com.lee989898.mysolelife.R
 import com.lee989898.mysolelife.databinding.FragmentHomeBinding
 
@@ -27,7 +28,19 @@ class HomeFragment : Fragment() {
         binding = FragmentHomeBinding.inflate(layoutInflater)
 
         binding.tipTap.setOnClickListener {
-            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+            it.findNavController().navigate(R.id.action_homeFragment_to_tipFragment)
+        }
+
+        binding.talkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_talkFragment)
+        }
+
+        binding.bookmarkTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_bookmarkFragment)
+        }
+
+        binding.storeTap.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_storeFragment)
         }
 
         return binding.root
