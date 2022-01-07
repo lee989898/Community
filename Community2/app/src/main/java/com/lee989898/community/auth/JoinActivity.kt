9e6da60303c970223a68b09eb8ece8781aec22a2
lee989898 <wnsrud1352@gmail.com>
2022-01-07 21:27:34 +0900

    commit

--- Community2/app/src/main/java/com/lee989898/community/auth/JoinActivity.kt
+++ Community2/app/src/main/java/com/lee989898/community/auth/JoinActivity.kt
@@ -1,14 +1,84 @@
 package com.lee989898.community.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.lee989898.community.MainActivity
 import com.lee989898.community.R
+import com.lee989898.community.databinding.ActivityJoinBinding
 
 class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    lateinit var binding: ActivityJoinBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+
+        auth = Firebase.auth
+
+        binding = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.joinBtn.setOnClickListener {
+
+            var isGoToJoin = true
+
+            val email = binding.emailArea.text.toString()
+            val password = binding.passwordArea.text.toString()
+            val passwordCheck = binding.passwordCheckArea.text.toString()
+
+            if (email.isEmpty()) {
+                Toast.makeText(this, "이메일을 입력해주세요", Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+            if (password.isEmpty()) {
+                Toast.makeText(this, "비밀번호를 입력해주세요", Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+            if (passwordCheck.isEmpty()) {
+                Toast.makeText(this, "비밀번호확인을 입력해주세요", Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+
+
+            if (password != passwordCheck) {
+                Toast.makeText(this, "비밀번호를 똑같이 입력해주세요", Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+
+            if (password.length < 6) {
+                Toast.makeText(this, "비밀번호를 6자리 이상 입력해주세요", Toast.LENGTH_SHORT).show()
+                isGoToJoin = false
+            }
+
+            if (isGoToJoin) {
+
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(this, "성공", Toast.LENGTH_SHORT).show()
+
+                            val intent = Intent(this, MainActivity::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
+
+                        } else {
+                            Toast.makeText(this, "실패", Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+            }
+
+
+        }
+
+
     }
 }
--- Community2/app/src/main/java/com/lee989898/community/auth/LoginActivity.kt
+++ Community2/app/src/main/java/com/lee989898/community/auth/LoginActivity.kt
@@ -3,12 +3,22 @@ package com.lee989898.community.auth
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lee989898.community.R
+import com.lee989898.community.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.emailArea.text.toString()
+            val password = binding.passwordArea.text.toString()
+        }
+
     }
 }
