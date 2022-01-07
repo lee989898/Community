cea4854f6f6075eb015ef117a9aab4bb4fc7645f
lee989898 <wnsrud1352@gmail.com>
2022-01-07 22:19:45 +0900

    commit

--- Community2/app/src/main/java/com/lee989898/community/SplashActivity.kt
+++ Community2/app/src/main/java/com/lee989898/community/SplashActivity.kt
@@ -4,20 +4,44 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.lee989898.community.auth.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this, IntroActivity::class.java))
-            finish()
+        auth = Firebase.auth
 
-        }, 2000)
+        if(auth.currentUser?.uid == null){
+            Log.d("SplashActivity", "null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, IntroActivity::class.java))
+                finish()
+
+            }, 2000)
+
+        }else{
+            Log.d("SplashActivity", "not null")
+
+            Handler().postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+
+            }, 2000)
+
+        }
+
+
 
 
     }
