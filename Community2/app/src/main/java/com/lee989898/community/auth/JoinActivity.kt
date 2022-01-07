e20bda4926127234f52a56c52e5736a1bbbbfd39
lee989898 <wnsrud1352@gmail.com>
2022-01-07 20:45:06 +0900

    commit

---
+++ Community2/app/src/main/java/com/lee989898/community/auth/JoinActivity.kt
@@ -0,0 +1,14 @@
+package com.lee989898.community.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lee989898.community.R
+
+class JoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
